fd5d8f7dc36bbeed56f10c495e656417b92acf57
Nigel Delaney <spamavoid@outlook.com>
2015-02-24 20:25:02 -0800

    R script to get multinomial regression parameters

--- src/R/PlotBinnedTraining.R
+++ src/R/PlotBinnedTraining.R
@@ -1,22 +1,19 @@
 library(ggplot2)
 library(scales)
-
+library(gridExtra)
+library(mlogit)
+library(nnet)
+library(reshape2)
 setwd('/Users/nigel/git/cafe-quality/NotTracked/')
 d= read.csv('Combined.csv')
 head(d)
 hist(d$Count)
 
 ms = grep(".Merge",colnames(d))
-ds = grep(".Dark", colnames(d))ds
-
+ds = grep(".Dark", colnames(d))
 d[,ds] = d[,ms] + d[ds]
 d = d[, -ms]
 
-gd = d[d$BP=="G",]
-head(dg)
-ggplot(gd, aes(x=SNR, y = GG.Stick))+geom_point()+geom_line()
-
-bp = "A"
 getData <-function(bp) {
   cd = d[d$BP==bp,]
   ctx1 = paste("N", bp, sep="")
@@ -49,10 +46,37 @@ pdf("BinnedSnrEstimates.pdf", width=12, height = 8)
 a = mkplot(colnames(nd)[4])
 b = mkplot(colnames(nd)[5])
 c = mkplot(colnames(nd)[6])
-d = mkplot(colnames(nd)[7])
-grid.arrange(a,b,c,d)
+dg = mkplot(colnames(nd)[7])
+grid.arrange(a,b,c,dg)
 dev.off()
 
 
+# Now to simulate and fit with a multinomial model
+a = getData("C")
+moves = c("Match","Branch","Dark","Stick")
+cd = a[a$Context=="CC",]
+results = c()
+contexts = levels(factor(nd$Context))
+ctx = "NA"
+for(i in 1:nrow(cd)) {
+  n=5000
+  outcomes = sample(moves,n, prob=cd[i,4:7], replace=TRUE)
+  td = data.frame(Context=factor(rep(ctx,n), levels= contexts), Outcome = outcomes, SNR=rep(cd$SNR[i],n))
+  results = rbind(results,td)
+}
+mod<-multinom(Outcome~SNR+I(SNR^2)+I(SNR^3), results)
+summary(mod)
+exp(coef(mod))
 
-do.call(grid.arrange,b)
+rng = range(results$SNR)
+tmpSNR = seq(rng[1], rng[2], .02)
+toy = data.frame(SNR=rep(tmpSNR,4),Outcome=rep(moves,each=length(tmpSNR)))
+hd = cbind(toy,predict(mod, newdata=toy, type="probs"))
+mhd = melt(hd[,-2], id.vars=c("SNR"), value.name="probability")
+ggplot(mhd,aes(x=SNR,y=probability,colour=variable))+geom_line()
+table(results$SNR,results$Outcome)
+#b = mlogit.data(results, shape="wide", choice="Outcome")
+#m <- mlogit(Outcome ~ SNR / 0 ,b)
+#summary(m)
+#head(results)
+
