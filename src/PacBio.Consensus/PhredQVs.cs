9a6223c959479087610d1dbbc7588fb876302f71
Nigel Delaney <spamavoid@outlook.com>
2015-03-02 10:20:30 -0800

    Add QVs class from PacBio.Utils
    Had to convert functions to use doubles but didn’t want to bring in the
    whole project.

---
+++ src/PacBio.Consensus/PhredQVs.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PacBio.Consensus
+{
+    public static class QVs
+    {
+        public static double CombineErrorProbability(IEnumerable<double> d)
+        {
+            var noErrorProb = 1.0;
+
+            foreach (var errorProb in d) 
+            {
+                noErrorProb *= (1.0 - errorProb);
+            }
+
+            return 1.0 - noErrorProb;
+        }
+
+        public static double CombineErrorProbability(params double[] d)
+        {
+            var noErrorProb = 1.0;
+
+            foreach (var errorProb in d) 
+            {
+                noErrorProb *= (1.0 - errorProb);
+            }
+
+            return 1.0 - noErrorProb;
+        }
+
+        public static double PhredProb(byte qv)
+        {
+            return Math.Pow(10, qv / -10.0);
+        }
+
+        public static double PhredProb(double qv)
+        {
+            return Math.Pow(10, qv / -10.0);
+        }
+
+        public static byte PhredQV(double pErr)
+        {
+            pErr = Math.Min(0.99, pErr);
+            // Cap QVs at 50 until we validate beyond that
+            return (byte)Math.Max(0, Math.Min(50, Math.Round(-10 * Math.Log10(pErr))));
+        }
+
+        public static byte ProbToQV(double pErr)
+        {
+            return PhredQV(pErr);
+        }
+
+        public static double QVToProb(byte qv)
+        {
+            return PhredProb(qv);
+        }
+    }
+}
+
