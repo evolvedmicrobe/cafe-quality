6a3e70e6659c619c6803e323803d2d6b362dadd6
Nigel Delaney <spamavoid@outlook.com>
2015-03-07 20:41:31 -0800

    Add class to debug dynamic programming in Xcode.

---
+++ src/ConsensusCore/src/C++/Quiver/MatrixTester.cpp
@@ -0,0 +1,37 @@
+//
+//  MatrixTester.cpp
+//  ConsensusCoreXCode
+//
+//  Created by Nigel Delaney on 3/7/15.
+//  Copyright (c) 2015 Pacific Biosciences. All rights reserved.
+//
+
+#include "MatrixTester.hpp"
+#include <fstream>
+#include <iostream>
+
+namespace ConsensusCore {
+    double  MatrixTester::TestMutationScorer()
+    {
+        std::ofstream myfile;
+        myfile.open("Temp.txt");
+        SNR snr(10.0,7.0,5.0,11.0);
+        ModelParams mp;
+        Read r("tester", "ACGTACGT");
+        ContextParameters ctx_params(snr);
+        TemplateParameterPair tpp("ACGTCGT", ctx_params);
+        QvEvaluator qv(r, tpp, mp);
+        SimpleQvSumProductRecursor bo(BandingOptions(4, -12.5));
+        myfile << "Made bits";
+        myfile.close();
+        SimpleQvSumProductMutationScorer t(qv, bo);
+        double score = t.Score();
+        return score;
+    }
+}
+
+int main() {
+    ConsensusCore::MatrixTester mt;
+    std::cout << mt.TestMutationScorer();
+    
+}
---
+++ src/ConsensusCore/src/C++/Quiver/MatrixTester.hpp
@@ -0,0 +1,22 @@
+//
+//  MatrixTester.h
+//  ConsensusCoreXCode
+//
+//  Created by Nigel Delaney on 3/7/15.
+//  Copyright (c) 2015 Pacific Biosciences. All rights reserved.
+//
+
+#ifndef __ConsensusCoreXCode__MatrixTester__
+#define __ConsensusCoreXCode__MatrixTester__
+
+#include <stdio.h>
+#include "Quiver/QuiverConfig.hpp"
+#include "Quiver/MutationScorer.hpp"
+
+namespace ConsensusCore {
+    class MatrixTester {
+    public:
+        double TestMutationScorer();
+    };
+}
+#endif /* defined(__ConsensusCoreXCode__MatrixTester__) */
