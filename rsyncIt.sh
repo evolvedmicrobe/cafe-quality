3aa421ee0972555b2d44e69bd379082dd4ceb695
Nigel Delaney <spamavoid@outlook.com>
2015-04-20 23:13:16 -0700

    Increase rsync speed

--- rsyncIt.sh
+++ rsyncIt.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rsync --verbose  --progress --stats --compress --rsh=/usr/bin/ssh \
       --recursive --times --perms --links --delete \
+      --exclude "NotTracked/*" \
       --exclude "*.exe" --exclude "*.dll" --exclude "*.mdb" --exclude "*.csv" \
       --exclude "*.pdf" --exclude "*fasta.gz" --exclude '*pptx' --exclude 'data' \
       --exclude 'lib' --exclude '*.o' --exclude '*.d' --exclude '*.a' --exclude '*.so' \
